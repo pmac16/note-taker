2f12572b798be2bfe11b43367d27fbe4565dbf70
Priya Macpherson <priyamacpherson@Priyas-MacBook-Pro.local>
2021-02-21 09:49:28 -0800

    working to debug error messagE

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const path = require('path');
+
 const app = express();
 
-// Set static folder
-app.use(express.static(path.join(__dirname, 'public')));
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
+// Set static folder
+// app.use(express.static(path.join(__dirname, 'public')));
 
 //HTML Routes
-app.get(`/`, (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 })
 
@@ -20,6 +24,16 @@ app.get('*', (req, res) => {
 })
 
 //API Routes
+// app.get('/api/notes', (req,res) => {
+//     return res.json(notes) 
+// });
+
+// app.post('/api/notes', (req, res) => {
+//     let newNote = req.body;
+//     console.log(newNote);
+//     notes.push(newNote);
+//     res.json(newNote);
+// });
 
 
 const PORT = process.env.PORT || 3001;
